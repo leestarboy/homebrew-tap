28e54951a4e90ab50e629ed164e38261e4e8aae4
leestarboy <leestarboy@outlook.com>
2023-02-10 14:01:52 +0800

    libass

---
+++ Formula/libass.rb
@@ -0,0 +1,67 @@
+class Libass < Formula
+  desc "Subtitle renderer for the ASS/SSA subtitle format"
+  homepage "https://github.com/libass/libass"
+  url "https://github.com/libass/libass/releases/download/0.17.0/libass-0.17.0.tar.xz"
+  sha256 "971e2e1db59d440f88516dcd1187108419a370e64863f70687da599fdf66cc1a"
+  license "ISC"
+
+  head do
+    url "https://github.com/libass/libass.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+  depends_on "fribidi"
+  depends_on "harfbuzz"
+  depends_on "libunibreak"
+  depends_on "fontconfig"
+
+  on_intel do
+    depends_on "nasm" => :build
+  end
+
+  def install
+    system "autoreconf", "-i" if build.head?
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
+    # libass uses coretext on macOS, fontconfig on Linux
+    args << "--disable-coretext"
+    args << "--enable-fontconfig"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "ass/ass.h"
+      int main() {
+        ASS_Library *library;
+        ASS_Renderer *renderer;
+        library = ass_library_init();
+        if (library) {
+          renderer = ass_renderer_init(library);
+          if (renderer) {
+            ass_renderer_done(renderer);
+            ass_library_done(library);
+            return 0;
+          }
+          else {
+            ass_library_done(library);
+            return 1;
+          }
+        }
+        else {
+          return 1;
+        }
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-I#{include}", "-L#{lib}", "-lass", "-o", "test"
+    system "./test"
+  end
+end
